e3485e2d1db09662d1efb201e36e02ea773486d7
Marouane Al Mandri <marouane.almandri@gmail.com>
2026-06-08 15:11:23 +0200

    Initial Presence app

---
+++ firebase-config.js
@@ -0,0 +1,10 @@
+// Paste your Firebase web app config here.
+// Firebase web config is public; security comes from Auth and Firestore rules.
+window.PRESENCE_FIREBASE_CONFIG = {
+  apiKey: "AIzaSyCQeLDX0wKU1Q0bQAM1FtD7aW_m6VTWEfo",
+  authDomain: "presence-app-b9717.firebaseapp.com",
+  projectId: "presence-app-b9717",
+  appId: "1:470449013799:web:e1d45e3a81362ca029fa02",
+  storageBucket: "presence-app-b9717.firebasestorage.app",
+  messagingSenderId: "470449013799"
+};
